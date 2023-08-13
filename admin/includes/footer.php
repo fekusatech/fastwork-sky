e35aece338607a6f44b43958d9152c38bf3bae17
Febri Kukuh <santosofebrikukuh@gmail.com>
2023-08-13 20:22:43 +0700

    Feat: adding file

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Dipersembahkan oleh<a href=""></a></b>
+    </div>
+    <strong>Copyright &copy; 2023 Sistem Absensi dan Penggajian</strong>
+</footer>
