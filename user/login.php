3540770be175ff0f870bf5d972bddf8f7463e665
Febri Kukuh <santosofebrikukuh@gmail.com>
2023-08-13 23:48:45 +0700

    Fix: login dinamyc

--- user/login.php
+++ user/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'includes/conn.php';
-
+$admin_url = str_replace("user", "admin", $base_url) . "index.php";
 if (isset($_POST['login'])) {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
@@ -20,19 +20,19 @@ if (isset($_POST['login'])) {
 			$row = $query->fetch_assoc();
 			$_SESSION['admin'] = $row['id'];
 			$_SESSION['data'] = $row;
-			header('location: '.$base_url.'admin/index.php');
+			header('location: index.php');
 		}
 	} else {
 		$row = $query->fetch_assoc();
 		if (password_verify($password, $row['password'])) {
 			$_SESSION['admin'] = $row['id'];
 			$_SESSION['data'] = $row;
-			header('location: admin/index.php');
+			echo "<script>window.location.href = '$admin_url';</script>";
+			exit;
 		} else {
 			$_SESSION['error'] = 'Incorrect password';
 		}
 	}
-	
 } else {
 	$_SESSION['error'] = 'Input admin credentials first';
 }
