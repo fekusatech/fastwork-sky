c7fb6961695b5f4c7108849c4ba61d05d85574dd
Febri Kukuh <santosofebrikukuh@gmail.com>
2023-08-13 23:30:24 +0700

    Fix: login user bisa untuk admin

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,8 +1,27 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'db_apsystem');
+$isSSL = function () {
+	if (!empty($_SERVER['https']))
+		return true;
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
+	if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_P
+ROTO'] == 'https')
+		return true;
+
+	if (!isset($_SERVER['SERVER_PORT']))
+		return false;
+
+	if ($_SERVER['SERVER_PORT'] == 443)
+		return true;
+
+	return false;
+};
+
+$prot = $isSSL() ? 'https://' : 'http://';
+// $config['base_url'] = $prot . $host . "/";
+$base_url = $prot . $_SERVER['HTTP_HOST'] .
+	str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$conn = new mysqli('localhost', 'root', '', 'db_apsystem');
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
--- user/login.php
+++ user/login.php
@@ -5,22 +5,34 @@ include 'includes/conn.php';
 if (isset($_POST['login'])) {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-
-	$sql = "SELECT * FROM employees WHERE employee_id = '$username' AND password='$password'";
+	//login admin dulu di cek
+	$sql = "SELECT * FROM admin WHERE username = '$username'";
 	$query = $conn->query($sql);
 
 	if ($query->num_rows < 1) {
-		$_SESSION['error'] = 'Cannot find account with the username';
+		//Jika tidak ada cek user biasa
+		$sql = "SELECT * FROM employees WHERE employee_id = '$username' AND password='$password'";
+		$query = $conn->query($sql);
+
+		if ($query->num_rows < 1) {
+			$_SESSION['error'] = 'Cannot find account with the username';
+		} else {
+			$row = $query->fetch_assoc();
+			$_SESSION['admin'] = $row['id'];
+			$_SESSION['data'] = $row;
+			header('location: '.$base_url.'admin/index.php');
+		}
 	} else {
 		$row = $query->fetch_assoc();
-		$_SESSION['admin'] = $row['id'];
-		$_SESSION['data'] = $row;
-		// if (password_verify($password, $row['password'])) {
-		// 	$_SESSION['admin'] = $row['id'];
-		// } else {
-		// 	$_SESSION['error'] = 'Incorrect password';
-		// }
+		if (password_verify($password, $row['password'])) {
+			$_SESSION['admin'] = $row['id'];
+			$_SESSION['data'] = $row;
+			header('location: admin/index.php');
+		} else {
+			$_SESSION['error'] = 'Incorrect password';
+		}
 	}
+	
 } else {
 	$_SESSION['error'] = 'Input admin credentials first';
 }
