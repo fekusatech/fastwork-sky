6ad64a721efa81936df79c4071f6f36a60e564b5
Febri Kukuh <santosofebrikukuh@gmail.com>
2023-09-02 14:11:09 +0700

    Fix: conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -22,8 +22,8 @@ $prot = $isSSL() ? 'https://' : 'http://';
 $base_url = $prot . $_SERVER['HTTP_HOST'] .
 	str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 if ($serverIP === 'localhost' || $serverIP === '127.0.0.1') {
-    // $conn = new mysqli('103.150.190.16', 'apsystem_db', 'apsystem_db', 'apsystem_db');
-	$conn = new mysqli('localhost', 'root', '', 'apsystem_db');
+    $conn = new mysqli('103.150.190.16', 'apsystem_db', 'apsystem_db', 'apsystem_db');
+	//$conn = new mysqli('localhost', 'root', '', 'apsystem_db');
 } else {
     $conn = new mysqli('localhost', 'root', '', 'apsystem_db');
 }
